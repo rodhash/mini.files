c3bc7986c2084fff3aa402dd7ce24a4acd1fbfd7
Hashimoto (Mini) <rodhash@gmail.com>
2026-06-20 21:52:54 -0300

    fix: center align default files icon
    Signed-off-by: Hashimoto (Mini) <rodhash@gmail.com>

--- lua/mini/files.lua
+++ lua/mini/files.lua
@@ -1233,11 +1233,11 @@ MiniFiles.default_prefix = function(fs_entry)
   -- Try falling back to 'nvim-web-devicons'
   if fs_entry.fs_type == 'directory' then return '   ', 'FloatTitle' end
   local has_devicons, devicons = pcall(require, 'nvim-web-devicons')
-  if not has_devicons then return ' ', 'MiniFilesFile' end
+  if not has_devicons then return '   ', 'MiniFilesFile' end
 
   local icon, hl = devicons.get_icon(fs_entry.name, nil, { default = false })
   -- return (' ' .. icon .. ' ' or '') .. ' ', hl or 'MiniFilesFile'
-  return (icon and (' ' .. icon .. ' ') or '') .. ' ', hl or 'MiniFilesFile'
+  return (icon and (' ' .. icon .. ' ') or '  ') .. ' ', hl or 'MiniFilesFile'
 end
 
 --- Default sort of file system entries
